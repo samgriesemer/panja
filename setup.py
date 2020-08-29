54ed36287ec354c5f10e632ba173dbe58f8f25fc
Sam Griesemer <samgriesemer@gmail.com>
2020-08-29 00:13:00 -0500

    add requirements to setup

--- setup.py
+++ setup.py
@@ -13,6 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/samgriesemer/panja",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'jinja2',
+        'pypandoc',
+        'tqdm',
+        'livereload'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
